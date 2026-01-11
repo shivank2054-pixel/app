9862ed502cb3729fa9336353c1225ef0823fabb3
shivank2054-pixel <shivank2054@gmail.com>
2026-01-10 23:20:31 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-import streamlit as st
+import streamlit as st 
 import joblib
 import numpy as np
 model = joblib.load("student_pass_model.pkl")
 st.title("Student Pass/Fail Prediction System")
-study_hours = st.number_input(
- "Study Hours per Week", min_value=0.0, max_value=40.0, value=10.0
-)
-attendance = st.number_input(
- "Attendance Percentage", min_value=0.0, max_value=100.0,
-value=75.0
-)
+study_hours = st.number_input("Study Hours per Week", min_value=0.0, max_value=40.0, value=10.0)
+attendance = st.number_input("Attendance Percentage", min_value=0.0, max_value=100.0,value=75.0)
 if st.button("Predict Result"):
  input_data = np.array([[study_hours, attendance]])
  prediction = model.predict(input_data)
