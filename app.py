58abf05c2eb3462f2e53c603ee53ab89def8a37f
shivank2054-pixel <shivank2054@gmail.com>
2026-01-10 23:27:49 -0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ attendance = st.number_input("Attendance Percentage", min_value=0.0, max_value=1
 if st.button("Predict Result"):
  input_data = np.array([[study_hours, attendance]])
  prediction = model.predict(input_data)
- if prediction[0] == 1:
+if prediction[0] == 1:
  st.success("Prediction: PASS")
- else:
+else:
  st.error("Prediction: FAIL")
+
